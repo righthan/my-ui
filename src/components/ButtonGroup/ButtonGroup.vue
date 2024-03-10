874d1c88734073849504bb976d583e0ff59e0856
righthan <2753875841@qq.com>
2024-03-10 16:18:23 +0800

    feat:add ButtonGroup componet

---
+++ src/components/ButtonGroup/ButtonGroup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="el-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'ElButtonGroup'
+})
+</script>
+
+<style scoped></style>
